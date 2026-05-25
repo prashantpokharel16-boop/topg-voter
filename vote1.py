13aca206059b22d7b7f3b187c162ed46de2c76fe
prashantpokharel16-boop <prashantpokharel16@gmail.com>
2026-05-25 07:20:46 +0545

    Update vote1.py

--- vote1.py
+++ vote1.py
@@ -1,7 +1,7 @@
 from playwright.sync_api import sync_playwright
 import time
 
-USERNAME = "SAROJGMG"
+USERNAME = "SAROJGMR"
 URL = "https://topg.org/minecraft-servers/server-455409"
 
 with sync_playwright() as p:
